3be4f08c8d294d7e7c65757f5f0627609cb2e4de
SimpleSandman <simple.sandman@gmail.com>
2021-03-17 09:49:06 -0500

    Bumped version number and added description

--- JsonFormatter/Properties/AssemblyInfo.cs
+++ JsonFormatter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JsonFormatter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Easy way to minify or prettify JSON")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JsonFormatter")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
